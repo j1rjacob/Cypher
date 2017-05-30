110482ec3f6acc1afc25a143c9d8c81e8606e81d
junar <junarjacob@yahoo.com>
2017-05-30 18:59:26 +0800

    Initial

---
+++ CypherMVC/CypherMVC/Models/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CypherMVC.Models
+{
+    [Bind(Exclude = "AssignedTo, AssociatedMessage, Category")]
+    public class Task
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime? DueDate { get; set; }
+        [Required]
+        public int AssignedToId { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public int AssociatedMessageId { get; set; }
+        public DateTime? Created { get; set; }
+        [StringLength(1000,MinimumLength =20)]
+        public string Notes { get; set; }
+        [Required]
+        public bool Completed { get; set; }
+
+        //Navigation Properties
+        public virtual Admin AssignedTo { get; set; }
+        public virtual Message AssociatedMessage { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
